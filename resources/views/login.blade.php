772fbd159bddd7a9344bbd37c5c96f3c398d37ef
arkajaya <hendraharyono0@gmail.com>
2025-12-15 11:27:06 +0700

    Creating Dashboard

---
+++ resources/views/login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Inventaris Pro - Manajemen Stok Modern</title>
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    {{-- Jika Anda menggunakan font custom seperti Poppins --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet">
+
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="antialiased font-sans bg-white text-gray-800">
+    <div class="min-h-screen">
+        
+    </div>
+</body>
+
+</html>
